09cc02c5c3de54ffe0a07516a530cca766ea57a8
o-rensa <131181504+O-rensa@users.noreply.github.com>
2025-02-23 00:04:43 +0800

    created custom event bus

--- jalikod-web-app/src/scripts/script.js
+++ jalikod-web-app/src/scripts/script.js
@@ -1,3 +1,52 @@
-function customAlert() {
-    alert("custom alert");
-}
+// app event bus
+const AppEvent = (() => {
+  const _callBacks = {};
+  
+  return {
+    on(eventName, callback) {
+      if (!_callBacks[eventName]) {
+        _callBacks[eventName] = [];
+      }
+    
+      _callBacks[eventName].push(callback);
+    },
+    
+    off(eventName, callback) {
+      const callbacks = _callBacks[eventName];
+      if (!callbacks) {
+        return
+      }
+      
+      let idx = -1;
+      for (let i = 0; i < callbacks.length; i++) {
+        if (callbacks[i] === callback) {
+          idx = i;
+          break;
+        }
+      }
+      
+      if (idx < 0) {
+        return
+      }
+      
+      _callBacks[eventName].splice(idx, 1);
+    },
+    
+    trigger(eventName) {
+      const callbacks = _callBacks[eventName];
+      if (!callbacks || !callbacks.length) {
+        return;
+      }
+      
+      var args = Array.prototype.slice.call(arguments, 1);
+      for (let i = 0; i < callbacks.length; i++) {
+        callbacks[i].apply(this, args);
+      }
+    }
+  }
+})();
+
+//export for node.js
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { AppEvent, }
+} 
--- jalikod-web-app/src/typings.d.ts
+++ jalikod-web-app/src/typings.d.ts
@@ -1,1 +1,9 @@
-declare function customAlert(): void;
+declare namespace TestFunc {
+  export function myAlert(message: string): void;
+}
+
+declare namespace AppEvent {
+  export function on(eventName: string, callback: () => void);
+  export function off(eventName: string, callback: () => void);
+  export function trigger(eventName: string);
+}
