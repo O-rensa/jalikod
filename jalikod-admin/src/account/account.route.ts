8f8dc91f6a5da0b171dbfc4cac732ff37005108b
O-rensa <131181504+O-rensa@users.noreply.github.com>
2025-04-18 12:53:03 +0800

    Initialized account layout

--- jalikod-admin/src/account/account.route.ts
+++ jalikod-admin/src/account/account.route.ts
@@ -1,3 +1,20 @@
 import { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
-export const ACCOUNTROUTES: Routes = [];
+export const ACCOUNTROUTES: Routes = [
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+  {
+  path: "login",
+  component: LoginComponent
+  },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  }
+];
+
--- jalikod-admin/src/root.routes.ts
+++ jalikod-admin/src/root.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const ROOTROUTES: Routes = [
   {
+    path: "account",
+    loadChildren: () => import("./account/account.route").then(m => m.ACCOUNTROUTES)
+  },
+  {
   path: "app",
   loadChildren: () => import("./app/app.routes").then(m => m.APPROUTES)
   },
